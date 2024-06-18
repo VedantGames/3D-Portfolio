1f38966c6148bd15307f28f15a1026dfedea4f9e
Vedant Games <guptavedant2549@gmail.com>
2024-06-18 17:16:13 +0530

    commits

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <div>
       <ParallaxLayer offset={4} speed={0.27} >
-        <div className='xl:px-[40rem] px-32 py-32 mt-[33rem]'>
+        <div className='2xl:px-[40rem] xl:px-[30rem] px-32 py-32 mt-[33rem]'>
           <div className='mb-10'>
             <h1 className='text-6xl font-bold'>
               Contact
